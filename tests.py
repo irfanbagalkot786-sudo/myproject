29ae236f6d2cd42bcfd525b98be546b116f90204
irfanbagalkot786-sudo <irfanbagalkot786@gmail.com>
2026-04-08 21:14:05 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+import whisper
+
+model = whisper.load_model("base")
+result = model.transcribe("C:\Users\Hp\OneDrive\Desktop\dummy proj\audio.mp3")
+
+print(result["text"])
