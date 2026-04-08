4fbbec189d703258b363087bb1c48e2c178495f2
irfanbagalkot786-sudo <irfanbagalkot786@gmail.com>
2026-04-08 20:39:52 +0530

    Refactor admin.py to streamline imports and registrations

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import StudentProfile,Skill, StudentSkill, LearningPath, Question, Assessment,Resume, ResumeAnalysis, Company, JobRole, Recommendation
+from .models import StudentProfile,Skill, StudentSkill, Question, Assessment,Resume, ResumeAnalysis, Company, JobRole, Recommendation
 
 class StudentProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'college', 'branch', 'cgpa', 'graduation_year')
@@ -9,7 +9,6 @@ admin.site.register(StudentProfile, StudentProfileAdmin)
 
 admin.site.register(Skill)
 admin.site.register(StudentSkill)
-admin.site.register(LearningPath)
 admin.site.register(Question)
 admin.site.register(Assessment)
 admin.site.register(Resume)
